d01a93622a2d044bae97b1c838e991ebfa5cd7a3
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-18 14:16:48 -0500

    implimention

---
+++ src/main/java/CRUDcarImplimentation.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class CRUDcarImplimentation implements CRUDcar {
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void UpdateCar(Car car) {
+
+    }
+
+    @Override
+    public void deleteCar(Car car) {
+
+    }
+
+    @Override
+    public Car getCarId() {
+        return null;
+    }
+
+    @Override
+    public List<Car> getAllCar() {
+        return null;
+    }
+}
